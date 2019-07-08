f4fefcf30e8bb0e260c8046221de35ae2b1dac2f
Loïc Norgeot <loic.norgeot@sketchfab.com>
2019-07-08 10:55:35 +0200

    [#D3D-4861] Adds the glTF exporter operator as bpy.ops.wm.sketchfab_export

--- addons/io_sketchfab_plugin/__init__.py
+++ addons/io_sketchfab_plugin/__init__.py
@@ -19,6 +19,7 @@
  * ***** END GPL LICENSE BLOCK *****
  """
 
+# General imports
 import os
 import urllib
 import requests
@@ -26,19 +27,26 @@ import threading
 import time
 from collections import OrderedDict
 
+# For the wireframe export
+import numpy as np
+import base64
+
+# Blender related
 import bpy
 import bpy.utils.previews
+from bpy_extras.io_utils import ExportHelper
 from bpy.props import (StringProperty,
                        EnumProperty,
                        BoolProperty,
                        IntProperty,
                        PointerProperty)
 
+# glTF and addon related
 from .io import *
 from .sketchfab import Config, Utils, Cache
 from .blender.imp.gltf2_blender_gltf import *
-
 from .blender.blender_version import Version
+from .io.exp import gltf2_io_draco_compression_extension
 
 
 # Blender 2.79 has been shipped with openssl version 0.9.8 which uses a TLS protocol
@@ -90,12 +98,8 @@ bl_info = {
 }
 bl_info['blender'] = getattr(bpy.app, "version")
 
-"""
-if bpy.app.version < (2, 80, 0):
-    bl_info["blender"] = (2, 79, 0)
-elif bpy.app.version == (2, 80, 0):
-    bl_info["blender"] = (2, 80, 0)
-"""
+def get_version_string():
+    return str(bl_info['version'][0]) + '.' + str(bl_info['version'][1]) + '.' + str(bl_info['version'][2])
 
 PLUGIN_VERSION = str(bl_info['version']).strip('() ').replace(',', '.')
 preview_collection = {}
@@ -356,12 +360,6 @@ class SketchfabLoginProps(bpy.types.PropertyGroup):
             if not self.password:
                 set_login_status('ERROR', 'Password is empty')
             bpy.ops.wm.sketchfab_login('EXEC_DEFAULT')
-
-    """
-    Version.assign_property(self, "email", StringProperty, name="email",
-    description="User email",
-    default="")
-    """
 
     vars()["email"] = StringProperty(
         name="email",
@@ -746,6 +744,420 @@ def parse_results(r, *args, **kwargs):
         skfb.skfb_api.prev_results_url = json_data['previous']
     else:
         skfb.skfb_api.prev_results_url = None
+
+
+"""
+The two following classes are a direct copy from the Khronos addon,
+  only adding an "export_wireframe" option to keep the topology information
+  in the glTF files.
+
+RUN with:
+bpy.ops.wm.sketchfab_export(filepath="/home/loic/toto.gltf", export_format="GLTF_EMBEDDED", export_wireframe=True, export_extras=True)
+"""
+
+class ExportGLTF2_Base:
+    # TODO: refactor to avoid boilerplate
+
+    def __init__(self):
+        self.is_draco_available = gltf2_io_draco_compression_extension.dll_exists()
+
+    bl_options = {'UNDO', 'PRESET'}
+
+    export_format = EnumProperty(
+        name='Format',
+        items=(('GLB', 'glTF Binary (.glb)',
+                'Exports a single file, with all data packed in binary form. '
+                'Most efficient and portable, but more difficult to edit later'),
+               ('GLTF_EMBEDDED', 'glTF Embedded (.gltf)',
+                'Exports a single file, with all data packed in JSON. '
+                'Less efficient than binary, but easier to edit later'),
+               ('GLTF_SEPARATE', 'glTF Separate (.gltf + .bin + textures)',
+                'Exports multiple files, with separate JSON, binary and texture data. '
+                'Easiest to edit later')),
+        description=(
+            'Output format and embedding options. Binary is most efficient, '
+            'but JSON (embedded or separate) may be easier to edit later'
+        ),
+        default='GLB'
+    )
+
+    ui_tab = EnumProperty(
+        items=(('GENERAL', "General", "General settings"),
+               ('MESHES', "Meshes", "Mesh settings"),
+               ('OBJECTS', "Objects", "Object settings"),
+               ('ANIMATION', "Animation", "Animation settings")),
+        name="ui_tab",
+        description="Export setting categories",
+    )
+
+    export_copyright = StringProperty(
+        name='Copyright',
+        description='Legal rights and conditions for the model',
+        default=''
+    )
+
+    export_image_format = EnumProperty(
+        name='Images',
+        items=(('NAME', 'Automatic',
+                'Determine the image format from the blender image name'),
+                ('JPEG', 'JPEG Format (.jpg)',
+                'Encode and save textures as .jpg files. Be aware of a possible loss in quality'),
+               ('PNG', 'PNG Format (.png)',
+                'Encode and save textures as .png files')
+               ),
+        description=(
+            'Output format for images. PNG is lossless and generally preferred, but JPEG might be preferable for web '
+            'applications due to the smaller file size'
+        ),
+        default='NAME'
+    )
+
+    export_texcoords = BoolProperty(
+        name='UVs',
+        description='Export UVs (texture coordinates) with meshes',
+        default=True
+    )
+
+    export_wireframe = BoolProperty(
+        name='Wireframe',
+        description='Export Wireframe for each primitive',
+        default=True
+    )
+
+    export_normals = BoolProperty(
+        name='Normals',
+        description='Export vertex normals with meshes',
+        default=True
+    )
+
+    export_draco_mesh_compression_enable = BoolProperty(
+        name='Draco mesh compression',
+        description='Compress mesh using Draco',
+        default=False
+    )
+
+    export_draco_mesh_compression_level = IntProperty(
+        name='Compression level',
+        description='Compression level (0 = most speed, 6 = most compression, higher values currently not supported)',
+        default=6,
+        min=0,
+        max=6
+    )
+
+    export_draco_position_quantization = IntProperty(
+        name='Position quantization bits',
+        description='Quantization bits for position values (0 = no quantization)',
+        default=14,
+        min=0,
+        max=30
+    )
+
+    export_draco_normal_quantization = IntProperty(
+        name='Normal quantization bits',
+        description='Quantization bits for normal values (0 = no quantization)',
+        default=10,
+        min=0,
+        max=30
+    )
+
+    export_draco_texcoord_quantization = IntProperty(
+        name='Texcoord quantization bits',
+        description='Quantization bits for texture coordinate values (0 = no quantization)',
+        default=12,
+        min=0,
+        max=30
+    )
+
+    export_tangents = BoolProperty(
+        name='Tangents',
+        description='Export vertex tangents with meshes',
+        default=False
+    )
+
+    export_materials = BoolProperty(
+        name='Materials',
+        description='Export materials',
+        default=True
+    )
+
+    export_colors = BoolProperty(
+        name='Vertex Colors',
+        description='Export vertex colors with meshes',
+        default=True
+    )
+
+    export_cameras = BoolProperty(
+        name='Cameras',
+        description='Export cameras',
+        default=False
+    )
+
+    export_selected = BoolProperty(
+        name='Selected Objects',
+        description='Export selected objects only',
+        default=False
+    )
+
+    export_extras = BoolProperty(
+        name='Custom Properties',
+        description='Export custom properties as glTF extras',
+        default=False
+    )
+
+    export_yup = BoolProperty(
+        name='+Y Up',
+        description='Export using glTF convention, +Y up',
+        default=True
+    )
+
+    export_apply = BoolProperty(
+        name='Apply Modifiers',
+        description='Apply modifiers (excluding Armatures) to mesh objects',
+        default=False
+    )
+
+    export_animations = BoolProperty(
+        name='Animations',
+        description='Exports active actions and NLA tracks as glTF animations',
+        default=True
+    )
+
+    export_frame_range = BoolProperty(
+        name='Limit to Playback Range',
+        description='Clips animations to selected playback range',
+        default=True
+    )
+
+    export_frame_step = IntProperty(
+        name='Sampling Rate',
+        description='How often to evaluate animated values (in frames)',
+        default=1,
+        min=1,
+        max=120
+    )
+
+    export_force_sampling = BoolProperty(
+        name='Always Sample Animations',
+        description='Apply sampling to all animations',
+        default=False
+    )
+
+    export_current_frame = BoolProperty(
+        name='Use Current Frame',
+        description='Export the scene in the current animation frame',
+        default=False
+    )
+
+    export_skins = BoolProperty(
+        name='Skinning',
+        description='Export skinning (armature) data',
+        default=True
+    )
+
+    export_all_influences = BoolProperty(
+        name='Include All Bone Influences',
+        description='Allow >4 joint vertex influences. Models may appear incorrectly in many viewers',
+        default=False
+    )
+
+    export_morph = BoolProperty(
+        name='Shape Keys',
+        description='Export shape keys (morph targets)',
+        default=True
+    )
+
+    export_morph_normal = BoolProperty(
+        name='Shape Key Normals',
+        description='Export vertex normals with shape keys (morph targets)',
+        default=True
+    )
+
+    export_morph_tangent = BoolProperty(
+        name='Shape Key Tangents',
+        description='Export vertex tangents with shape keys (morph targets)',
+        default=False
+    )
+
+    export_lights = BoolProperty(
+        name='Punctual Lights',
+        description='Export directional, point, and spot lights. '
+                    'Uses "KHR_lights_punctual" glTF extension',
+        default=False
+    )
+
+    export_displacement = BoolProperty(
+        name='Displacement Textures (EXPERIMENTAL)',
+        description='EXPERIMENTAL: Export displacement textures. '
+                    'Uses incomplete "KHR_materials_displacement" glTF extension',
+        default=False
+    )
+
+    will_save_settings = BoolProperty(
+        name='Remember Export Settings',
+        description='Store glTF export settings in the Blender project',
+        default=False)
+
+    # Custom scene property for saving settings
+    scene_key = "glTF2ExportSettings"
+
+    # Helper functions
+    def get_meshes_to_export(self, scene):
+        """
+        Returns a list of meshes to export, according to selection filters.
+        For the first test, only considers the objects in the scene.
+        Future should take view_layers, hidden objects... into consideration
+        """
+        TOEXPORT = []
+        for o in scene.objects:
+            if o.type=="MESH":
+                if self.export_selected:
+                    if bpy.app.version < (2, 80, 0):
+                        if o.select:
+                            TOEXPORT.append(o)
+                    else:
+                        if o.select_get():
+                            TOEXPORT.append(o)
+                else:
+                    TOEXPORT.append(o)
+        return TOEXPORT
+    def activate_wireframe(self, obj):
+        # Number of edges
+        nEdges = len(obj.data.edge_keys)
+        obj.data["nEdges"] = nEdges
+        # Edges
+        edges = np.array(obj.data.edge_keys, dtype=np.uint32).reshape((2*nEdges,))
+        obj.data["wireframe"] = str(base64.b64encode(edges))
+        # Data type (32bits uint)
+        obj.data["componentType"] = 5125
+    def deactivate_wireframe(self, obj):
+        try: # Because sometimes things might not go as planned
+            del obj.data["nEdges"]
+            del obj.data["wireframe"]
+            del obj.data["componentType"]
+        except:
+            print("Warning: Problem while deleting the custom properties for %s" % o.name)
+    def save_settings(self, context):
+        # find all export_ props
+        all_props = self.properties
+        export_props = {x: getattr(self, x) for x in dir(all_props)
+                        if x.startswith("export_") and all_props.get(x) is not None}
+
+        context.scene[self.scene_key] = export_props
+
+    # INVOKE and EXECUTE, but no draw as only used in backend processing
+    def invoke(self, context, event):
+        settings = context.scene.get(self.scene_key)
+        self.will_save_settings = False
+        if settings:
+            try:
+                for (k, v) in settings.items():
+                    setattr(self, k, v)
+                self.will_save_settings = True
+
+            except (AttributeError, TypeError):
+                self.report({"ERROR"}, "Loading export settings failed. Removed corrupted settings")
+                del context.scene[self.scene_key]
+
+        return ExportHelper.invoke(self, context, event)
+
+    def execute(self, context):
+        import os
+        import datetime
+        from .blender.exp import gltf2_blender_export
+
+        if self.will_save_settings:
+            self.save_settings(context)
+
+        if self.export_format == 'GLB':
+            self.filename_ext = '.glb'
+        else:
+            self.filename_ext = '.gltf'
+
+        # All custom export settings are stored in this container.
+        export_settings = {}
+
+        export_settings['timestamp'] = datetime.datetime.now()
+
+        export_settings['gltf_filepath'] = bpy.path.ensure_ext(self.filepath, self.filename_ext)
+        export_settings['gltf_filedirectory'] = os.path.dirname(export_settings['gltf_filepath']) + '/'
+
+        export_settings['gltf_format'] = self.export_format
+        export_settings['gltf_image_format'] = self.export_image_format
+        export_settings['gltf_copyright'] = self.export_copyright
+        export_settings['gltf_texcoords'] = self.export_texcoords
+        export_settings['gltf_normals'] = self.export_normals
+        export_settings['gltf_tangents'] = self.export_tangents and self.export_normals
+
+        if self.is_draco_available:
+            export_settings['gltf_draco_mesh_compression'] = self.export_draco_mesh_compression_enable
+            export_settings['gltf_draco_mesh_compression_level'] = self.export_draco_mesh_compression_level
+            export_settings['gltf_draco_position_quantization'] = self.export_draco_position_quantization
+            export_settings['gltf_draco_normal_quantization'] = self.export_draco_normal_quantization
+            export_settings['gltf_draco_texcoord_quantization'] = self.export_draco_texcoord_quantization
+        else:
+            export_settings['gltf_draco_mesh_compression'] = False
+
+        export_settings['gltf_materials'] = self.export_materials
+        export_settings['gltf_colors'] = self.export_colors
+        export_settings['gltf_cameras'] = self.export_cameras
+        export_settings['gltf_selected'] = self.export_selected
+        export_settings['gltf_layers'] = True  # self.export_layers
+        export_settings['gltf_extras'] = self.export_extras
+        export_settings['gltf_yup'] = self.export_yup
+        export_settings['gltf_apply'] = self.export_apply
+        export_settings['gltf_current_frame'] = self.export_current_frame
+        export_settings['gltf_animations'] = self.export_animations
+        if self.export_animations:
+            export_settings['gltf_frame_range'] = self.export_frame_range
+            export_settings['gltf_force_sampling'] = self.export_force_sampling
+        else:
+            export_settings['gltf_frame_range'] = False
+            export_settings['gltf_move_keyframes'] = False
+            export_settings['gltf_force_sampling'] = False
+        export_settings['gltf_skins'] = self.export_skins
+        if self.export_skins:
+            export_settings['gltf_all_vertex_influences'] = self.export_all_influences
+        else:
+            export_settings['gltf_all_vertex_influences'] = False
+        export_settings['gltf_frame_step'] = self.export_frame_step
+        export_settings['gltf_morph'] = self.export_morph
+        if self.export_morph:
+            export_settings['gltf_morph_normal'] = self.export_morph_normal
+        else:
+            export_settings['gltf_morph_normal'] = False
+        if self.export_morph and self.export_morph_normal:
+            export_settings['gltf_morph_tangent'] = self.export_morph_tangent
+        else:
+            export_settings['gltf_morph_tangent'] = False
+
+        export_settings['gltf_lights'] = self.export_lights
+        export_settings['gltf_displacement'] = self.export_displacement
+
+        export_settings['gltf_binary'] = bytearray()
+        export_settings['gltf_binaryfilename'] = os.path.splitext(os.path.basename(
+            bpy.path.ensure_ext(self.filepath,self.filename_ext)))[0] + '.bin'
+
+        # Exporting the object, with the wireframe
+        if self.export_wireframe:
+            to_export = self.get_meshes_to_export(context.scene)
+            for o in to_export:
+                self.activate_wireframe(o)
+            res = gltf2_blender_export.save(context, export_settings)
+            for o in to_export:
+                self.deactivate_wireframe(o)
+            return res
+        # Or without the wireframe (fallback on the original method)
+        else:
+            return gltf2_blender_export.save(context, export_settings)
+
+class ExportGLTF2(bpy.types.Operator, ExportGLTF2_Base, ExportHelper):
+    """Export scene as glTF for Sketchfab processing"""
+    bl_idname = 'wm.sketchfab_export'
+    bl_label = 'Export glTF 2.0'
+
+    filename_ext = ''
+
+    filter_glob = StringProperty(default='*.glb;*.gltf', options={'HIDDEN'})
 
 
 class ThumbnailCollector(threading.Thread):
@@ -1328,6 +1740,7 @@ classes = (
     ViewOnSketchfab,
     SketchfabDownloadModel,
     SketchfabLogger,
+    ExportGLTF2,
     )
 
 def check_plugin_version(request, *args, **kwargs):
